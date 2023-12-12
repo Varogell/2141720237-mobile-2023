5b687454f5b27d679c170d79846b31bc7ba6d0be
Varogell <alvaroivanka@gmail.com>
2023-12-12 08:46:52 +0700

    praktikum2

--- week13/docs/src/lanjutanstate/lib/main.dart
+++ week13/docs/src/lanjutanstate/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 void main() {
   runApp(const MyApp());
 }
@@ -30,14 +32,30 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
   
   @override
   void initState() {
-    super.initState();
-    colorStream = ColorStream();
-    changeColor();
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    }).onError((error){
+      setState((){
+        lastNumber = -1;
+      });
+    });
   }
   @override
+    void dispose() {
+    numberStreamController.close();
+    super.dispose();
+  }
    void changeColor() async {
     await for (var eventColor in colorStream.getColors()){
       setState(() {
@@ -45,6 +63,12 @@ class _StreamHomePageState extends State<StreamHomePage> {
       });
     }
    }
+   void addRandomNumber() {
+  Random random = Random();
+  // int myNum = random.nextInt(10);
+  // numberStream.addNumberToSink(myNum);
+  numberStream.addError();
+}
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +76,20 @@ class _StreamHomePageState extends State<StreamHomePage> {
        appBar: AppBar(
         title: const Text('Alvaro Stream'),
       ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
-      ),
-    );
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center, 
+           children: [
+            Text(lastNumber.toString()),
+            ElevatedButton(
+              onPressed: () => addRandomNumber(),
+              child: const Text('New Random Number'),
+            )
+           ],
+          ),
+    ));
   }
 
 }
